ca387c1ef67f661a407b736953843bec765e2827
chensayha <54895690+chensayha@users.noreply.github.com>
2020-05-31 00:57:18 +0800

    5/31 change

--- assets/script/faq.js
+++ assets/script/faq.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
     $(function () { 
         $('.faqList').click(function () { 
-            $('.answer').removeClass('active');
-            $(this).children('.answer').slideToggle().addClass('active');
+            $(this).children('.answer').toggleClass('active').slideToggle();
         }); 
     });
   });
-
